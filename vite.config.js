ce78d44f71dd3cb250a133270e9927d5e590074d
BilelMat <matmouribilell@gmail.com>
2024-01-31 11:09:20 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000, // pour que l'appli se lance dans le port 3000 et plus 5173
+  },
+  plugins: [react()],
+});
